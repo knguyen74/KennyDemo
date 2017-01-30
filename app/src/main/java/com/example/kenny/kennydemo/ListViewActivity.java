3e94da7af9073cc70235134a6ef226fa5b0c9a75
knguyen74 <knguyen7495@gmail.com>
2017-01-30 17:38:00 -0500

    update 1

---
+++ app/src/main/java/com/example/kenny/kennydemo/ListViewActivity.java
@@ -0,0 +1,23 @@
+package com.example.kenny.kennydemo;
+import android.widget.ListView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.kenny.kennydemo.adapter.ListViewAdapter;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+        initialView();
+    }
+
+    private void initialView() {
+        listView = (ListView) findViewById(R.id.list_view);
+        ListViewAdapter listViewAdapter = new ListViewAdapter(this);
+        listView.setAdapter(listViewAdapter);
+    }
+}
